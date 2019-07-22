c455632e6bca041f20163af1459161175bd4c5f1
hiran@unicorn-solutions.com <hiran@unicorn-solutions.com>
2019-07-22 15:01:08 +0530

    https issues

--- Dockerfile
+++ Dockerfile
@@ -3,12 +3,13 @@ RUN mkdir -p /var/auth-server
 COPY ./ /var/auth-server
 ADD odbcinst.ini /etc/odbcinst.ini
 RUN apt-get update
+RUN apt-get install -y mssql-tools
 RUN apt-get install -y gcc g++
 RUN apt-get install -y tdsodbc unixodbc-dev --no-install-recommends apt-utils
 RUN apt install unixodbc-bin -y --no-install-recommends apt-utils
 RUN apt-get clean -y
 RUN pip install -r /var/auth-server/requirements.txt
-ENV auth_db_connection="Driver={xxx};Server=192.168.99.145;Database=xxx;Uid=xxx;Pwd=xxx;"
+ENV auth_db_connection=""
 
 ENTRYPOINT python /var/auth-server/app.py
 EXPOSE 8083
--- apis/__init__.py
+++ apis/__init__.py
@@ -1,4 +1,5 @@
 from flask_restplus import Api
+from flask import url_for
 from apis.Health import health
 from apis.auth import auth
 
@@ -11,7 +12,17 @@ authorizations = {
         'description': 'Enter your bearer token in the format **Bearer &lt;token>**'
     }
 }
-api = Api(version='1.0', title='Unicorn authantication API',
+
+class MyApi(Api):
+    @property
+    def specs_url(self):
+        """Monkey patch for HTTPS"""
+        scheme = 'https' if '443' in self.base_url else 'http'
+        return url_for(self.endpoint('specs'), _external=True, _scheme=scheme)
+
+
+
+api = MyApi(version='1.0', title='Unicorn authantication API',
     description='token server',
     authorizations=authorizations,
     security='apikey'
