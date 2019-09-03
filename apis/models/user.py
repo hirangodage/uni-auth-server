521f546f5bcf3737ad5a503fdb51049f3c7968ba
hiran@unicorn-solutions.com <hiran@unicorn-solutions.com>
2019-09-03 17:46:35 +0530

    fix

--- apis/models/user.py
+++ apis/models/user.py
@@ -25,23 +25,16 @@ adminUser={
     'ex3': fields.String(required=False, description='extended filed')
 }
 
-client={
+def newClientModel(ns):
+     return ns.model('client',{
     'name': fields.String(required=True, description='tenant name'),
     'des': fields.String(required=True, description='description'),
     'aud':fields.String(required=True, description='comma separated list of audiances'),
-    'adminUser':fields.Nested({
-    'username': fields.String(required=True, description='username'),
-    'password': fields.String(required=True, description='password'),
-    'email': fields.String(required=True, description='email address'),
-    'mobile': fields.String(required=False, description='mobile number'),
-    'ex1': fields.String(required=False, description='extended filed'),
-    'ex2': fields.String(required=False, description='extended filed'),
-    'ex3': fields.String(required=False, description='extended filed'),
-}),
+    'adminUser':fields.Nested(newUserModel(ns)),
     'ex1': fields.String(required=False, description='extended filed'),
     'ex2': fields.String(required=False, description='extended filed'),
     'ex3': fields.String(required=False, description='extended filed')
-}
+});
 
 aud={
     'clientid': fields.String(required=True, description='client name'),
