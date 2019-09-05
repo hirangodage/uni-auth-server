d2094fd37a8cb88ac33abdf4b449fee4481ca013
hiran@unicorn-solutions.com <hiran@unicorn-solutions.com>
2019-09-05 12:40:53 +0530

    json issue

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ ar=[{'Status': 1, 'Name': 'hiran', 'Email': 'hiran@tl.lk', 'Mobile': '12', 'Role
  'aud': 'admin', 'AudKey': 'test@key', 'Expire': 50000000, 'uex1': 'tl', 'uex2': None, 'uex3': None,
   'aex1': None, 'aex2': None, 'aex3': None}]
 mo = map(lambda x:x['Role'],list(ar))
-
-res = getClients('fdc')
-print(json.dumps({'res':res}))
+rest = []
+rest.append(dict(zip('name','hiran')))
+res = getUser('','','')
+print(json.dumps(res, sort_keys=True, default=str))
